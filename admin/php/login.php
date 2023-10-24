b7f55dc919843bf8c3c1ff296625c1321e623c2b
Gerald Lee <67699594+geraldlee2001@users.noreply.github.com>
2023-10-24 18:02:20 +0800

    Feat/db connection (#7)
    * feat/db connection
    
    * feat/db

--- admin/php/login.php
+++ admin/php/login.php
@@ -1,19 +1,31 @@
 <!-- process_login.php -->
 <?php
+include '../../databaseConnection.php';
 // Check if the username and password match (replace with your authentication logic)
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if ($username === 'admin' && $password === 'admin') {
-  // Set a cookie if login is successful
-  setcookie('user', $username); // Cookie expires in 1 hour
-  header('Location: ../'); // Redirect to a welcome page
-  setcookie("user", $username, time() + 3600, "/", "localhost");
+// Securely hash the user's password and store it in the database
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-  exit;
+// Query the database to retrieve the user's information
+$query = "SELECT * FROM user WHERE userName = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+if ($user && $password === $user['password']) {
+  echo $user['type'];
+  if ($user['type'] === 'ADMIN') {
+    setcookie('user', $username); // Cookie expires in 1 hour
+    header('Location: ../'); // Redirect to a welcome page
+    setcookie("user", $username, time() + 3600, "/", "localhost");
+  } else {
+    // User is not an admin
+    echo "You are not an admin.";
+  }
 } else {
-  // Redirect back to the login page if login fails
-  header('Location: ../login.php');
-  exit;
+  // Invalid login credentials
+  echo "Invalid username or password.";
 }
-?>
