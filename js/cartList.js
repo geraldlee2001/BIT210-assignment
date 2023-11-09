e7924a9f7c6a98d1a025be173ef177be11f9e0da
Florence0416 <fadksvflorence@gmail.com>
2023-11-09 22:13:21 +0800

    cartList amend

--- js/cartList.js
+++ js/cartList.js
@@ -1,24 +1,20 @@
-// Add your custom JavaScript here
-      // For example, you might want to add functionality for the plus and minus buttons
+var quantityButtons = document.querySelectorAll('.btn-link');
 
-      // Get all buttons with class 'btn-link'
-      var quantityButtons = document.querySelectorAll('.btn-link');
 
-      // Loop through each button and add click event listener
-      quantityButtons.forEach(function (button) {
-        button.addEventListener('click', function () {
-          // Find the input element related to the clicked button
-          var inputElement = this.parentNode.querySelector('input[type=number]');
+quantityButtons.forEach(function (button) {
+button.addEventListener('click', function () {
 
-          // Check if the input element exists
-          if (inputElement) {
-            if (this.classList.contains('px-2')) {
-              // If the button is minus, decrement the value
-              inputElement.stepDown();
-            } else {
-              // If the button is plus, increment the value
-              inputElement.stepUp();
-            }
-          }
-        });
-      });
+    var inputElement = this.parentNode.querySelector('input[type=number]');
+
+
+    if (inputElement) {
+    if (this.classList.contains('px-2')) {
+        // If the button is minus, decrement the value
+        inputElement.stepDown();
+    } else {
+        // If the button is plus, increment the value
+        inputElement.stepUp();
+    }
+    }
+});
+});
