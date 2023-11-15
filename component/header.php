f7b8919bd269ade635f389ffee1289d9ac509363
B2000513 <B2000513@helplive.edu.my>
2023-11-15 20:55:28 +0800

    fix: logout button no come out put in logo but still very cacat

--- component/header.php
+++ component/header.php
@@ -3,7 +3,7 @@
   ?>
  <nav class="navbar navbar-expand-lg navbar-dark fixed-top " id="mainNav">
    <div class="container">
-     <a class="navbar-brand" href="../"><img src="assets/img/navbar-logo.svg" alt="..." /></a>
+     <a class="navbar-brand" href="../"><img src="../images/Logo Creator (Community) (1).png" alt="..." width="100" height="200"/></a>
      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
        Menu
        <i class="fas fa-bars ms-1"></i>
